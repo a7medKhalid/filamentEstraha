b0c43098a9c85b76a78f2458dc6a4724950511b4
a7medKhalid <akalghamdi@uqu.edu.sa>
2023-11-12 10:24:14 +0300

    custom page to show all prices

---
+++ app/Filament/Pages/Prices.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\Price;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+
+class Prices extends Page implements HasForms, HasTable
+{
+    use InteractsWithTable;
+    use InteractsWithForms;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.prices';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Price::query()->with('estraha'))
+            ->columns([
+                TextColumn::make('price')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('start_date')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('end_date')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('estraha.name')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                Filter::make('Date')
+                    ->form([
+                        DatePicker::make('start_date')
+                            ->label('Start Date')
+                            ->required()
+                            ->firstDayOfWeek(1)
+                            ->format('YYYY-MM-DD')
+                            ->default(now()->startOfMonth()),
+                        DatePicker::make('end_date')
+                            ->label('End Date')
+                            ->required()
+                            ->firstDayOfWeek(1)
+                            ->format('YYYY-MM-DD')
+                            ->default(now()->endOfMonth()),
+                        ]
+                    )
+                    ->query(function (Builder $query, array $data){
+                        $query->whereBetween('start_date', [$data['start_date'], $data['end_date']]);
+                    }),
+            ]);
+    }
+}
